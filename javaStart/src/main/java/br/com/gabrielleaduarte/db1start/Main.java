d5a11b02d429a35ab78fe91128a8e226e7074065
gabrielleaduarte <gabrielleaduarte>
2021-05-03 13:37:09 -0300

    Deletando o .idea

--- javaStart/src/main/java/br/com/gabrielleaduarte/db1start/Main.java
+++ javaStart/src/main/java/br/com/gabrielleaduarte/db1start/Main.java
@@ -4,11 +4,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        Scanner entrada = new Scanner(System.in);
-
-        int a = entrada.nextInt();
-        int b = entrada.nextInt();
-
-        System.out.println("X = " + (a+b));
+        System.out.println("Olá, mundo!");
     }
 }
